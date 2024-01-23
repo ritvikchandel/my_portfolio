9b876f51fff807ee71f7606ea8d029b14d298a3c
unknown <trader.ritvik@gmail.com>
2024-01-23 16:43:26 +0530

    my-portfolio: Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.header-mob')
+    sidebar.style.display = 'flex'
+}
+
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.header-mob')
+    sidebar.style.display = 'none'
+}
